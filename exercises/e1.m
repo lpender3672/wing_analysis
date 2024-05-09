f2bc036a4852d9c092388f30fb5c5d571e2b46cf
L.W. Pender <lwp26@cam.ac.uk>
2024-05-09 12:48:09 +0100

    finished e1 without vectors

--- exercises/e1.m
+++ exercises/e1.m
@@ -1,14 +1,30 @@
 
-disp("Hello World!")
-x = linspace(-2.5, 2.5, 1000);
-y = linspace(-2, 2, 1000);
+clear
+close all;
 
-MGRID = meshgrid(x,y);
+disp("Hello World!")
+x = linspace(-2.5, 2.5, 101);
+y = linspace(-2, 2, 51);
 
 xc = 0.5;
 yc = 0.25;
 Gamma = 3.0;
 
-function psi = psimat(x, y)
-psi = 0
+[xm, ym] = meshgrid(x, y);
+PSI = psimat(x, y, xc, yc, Gamma);
+
+
+c = -0.4:0.2:1.2;
+contour(xm,ym,PSI',c);
+
+axis image;
+
+function psi = psimat(xarr, yarr, xc, yc, Gamma)
+    psi = zeros(length(xarr), length(yarr));
+
+    for i=1:length(xarr)
+        for j=1:length(yarr)
+            psi(i, j) = psipv(xc, yc, Gamma, xarr(i), yarr(j));
+        end
+    end
 end
