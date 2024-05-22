763accefdcef639c6bbf2424d762f5cbcefb0628
JRT03 <98089743+JRT03@users.noreply.github.com>
2024-05-22 17:23:56 +0100

    Revert "Merge branch 'master' of https://github.com/lpender3672/wing_analysis"
    This reverts commit 508cd3354997fc334de7fd629b642a3e3aaa3335, reversing
    changes made to b30cf2d5d7d86081525649dcdf5c54e6f1d2611d.

--- exercises/week2/w2e4.m
+++ exercises/week2/w2e4.m
@@ -3,39 +3,31 @@ close all
 
 global Re_L ue0 due_dx
 
-% Definition of global variables to be accessed in thickdash
 ue0 = 1;
 Re_L = 10^7;
 due_dx = 0; 
 
-% Calculation of initial variables for ODE solver
 x0 = 0.01;
 thick0(1) = 0.023*x0*(Re_L*x0)^(-1/6);
 thick0(2) = 1.83*thick0(1);
 
-% Solving ODE for x/L in range 0 to 0.99
 [delx thickhist] = ode45(@thickdash, [0 0.99], thick0);
 
-% Adding initial x0 offset so range is 0.01 to 1.0
 x = x0 + delx;
 
-% Calculating power law estimates of profiles in vector form
 theta7_over_L = 0.037 .* x .* (Re_L .* x) .^ (-1/5);
 theta9_over_L = 0.023 .* x .* (Re_L .* x) .^ (-1/6);
 
-% Plotting momentum thickness profiles
 hold on;
+
 plot(x, thickhist(:,1));
 plot(x, theta7_over_L);
 plot(x, theta9_over_L);
 
-legend("Exact", "1/7th Est.", "1/9th Est.", 'location', 'northwest')
+legend("Exact", "1/7th Est.", "1/9th Est.")
 hold off;
 grid on;
 
 xlabel('$\frac{x}{L}$', 'Interpreter', 'latex', 'FontSize', 20) 
 ylabel('$\frac{\theta}{L}$', 'Interpreter', 'latex', 'FontSize', 20)
-%title('Momentum Thickness Solution and Power Law Estimates')
-
-print -deps2c exercises\week2\figures\w2e4.eps
-
+title('Momentum Thickness Solution and Power Law Estimates')
