b13434f5d03988a3c28fff2f6e5b593222324c9f
lpender3672 <58957722+lpender3672@users.noreply.github.com>
2024-05-27 16:07:12 +0100

    forces done

--- exercises/week3/forces.m
+++ exercises/week3/forces.m
@@ -2,10 +2,13 @@ function [cl cd] = forces(circ,cp,delstarl,thetal,delstaru,thetau)
 
    cl = -2 * circ;
    
-   He_l = delstarl(end) / thetal(end);
-   He_u = delstaru(end) / thetau(end);
+   H_l = delstarl(end) / thetal(end);
+   H_u = delstaru(end) / thetau(end);
+   ue = sqrt(1-cp(end));
    
-   theta_ifty_l = thetal(end)
-   
+   theta_ifty_l = thetal(end) * (ue ^ ((H_l + 5)/2));
+   theta_ifty_u = thetau(end) * (ue ^ ((H_u + 5)/2));
 
+   cd = 2 * (theta_ifty_l + theta_ifty_u);
+   
 end
